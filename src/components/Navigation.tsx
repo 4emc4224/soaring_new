69afa1ee227238d66d084b5f34fd82c5e60dfdab
4emc4224 <gotofobiu@gmail.com>
2025-06-30 01:09:32 +0300

    Update Header Navigation and Size

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 const Navigation: React.FC = () => {
-  const navItems = ['Competition', 'Leaderboard', 'Winners', 'Play Together', 'Roulette'];
+  const navItems = ['Home', 'Media', 'Roulette'];
 
   return (
-    <nav className="flex items-center justify-center space-x-4">
+    <nav className="flex items-center justify-center space-x-6">
       {navItems.map((item) => (
         <a
           key={item}
-          href={`#${item.toLowerCase().replace(' ', '-')}`}
+          href={`#${item.toLowerCase()}`}
           className="nav-link-minimal"
         >
           {item}
